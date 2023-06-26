551c9207557b8b857e8453d29b985dbbb8c5a7d4
eunjoong kim <66717979+ejinhvn-0112@users.noreply.github.com>
2023-06-27 00:47:50 +0900

    Initial Commit

---
+++ VisionOS_test/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  VisionOS_test
+//
+//  Created by 김은중 on 2023/06/27.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionOS_test/VisionOS_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS_testApp.swift
+//  VisionOS_test
+//
+//  Created by 김은중 on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
